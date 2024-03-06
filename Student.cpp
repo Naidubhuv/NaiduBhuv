ee437729b4545cedbb5b4ade013af6269e0c2295
Naidubhuv <160043167+Naidubhuv@users.noreply.github.com>
2024-03-06 22:47:08 +0530

    Create Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+        string fullName;
+        int rollNum;
+        double semPercentage;
+        string collegeName;
+        int collegeCode;
+        Student()
+        {
+            cout<<"Student class initialized!!\nIam a student\n";
+        }
+        ~Student(){}
+};
+
+int main()
+{
+    Student venkat;
+    cout<<"Enter your name : ";
+    getline(cin,venkat.fullName);
+    cout<<"Enter your roll number : ";
+    cin>>venkat.rollNum;
+    cout<<"Enter your sem percentage : ";
+    cin>>venkat.semPerentage;
+    cout<<"Enter your college name : ";
+    cin>>venkat.collegeName;
+    cout<<"Enter your college code : ";
+    cin>>venkat.collegeCode;
+    cout<<"Name : "<<venkat.fullName<<endl;
+    cout<<"RollNum : "<<venkat.rollNum<<endl;
+    cout<<"SemPercentage : "<<venkat.semPercentage<<endl;
+    cout<<"CollegeName : "<<venkat.collegeName<<endl;
+    cout<<"CollegeCode : "<<venkat.collegeCode<<endl;
+    return 0;
+}
